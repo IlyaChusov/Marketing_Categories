01b8067e197753e594a956a809ddf1ab42897143
Ilya Chusov <ilyachusov@gmail.com>
2023-11-24 15:30:48 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Новостной парсер Arena</title>
+    <link rel="icon" href="../assets/images/favicon.ico" type="image/x-icon">
+    <link type="text/css" rel="stylesheet" href="../assets/libraries/materialize.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/main.css">
+
+
+</head>
+<body>
+
+<div class="all">
+    <div class="center_block">
+        <div class="container">
+            <h3>
+                Новостной парсер
+            </h3>
+
+            <p></p>
+            <div id="start_div"></div>
+            <p></p>
+
+            <button type="submit" class="upload_button btn waves-effect waves-light">
+                <span class="upload_text">Запросить</span>
+                <i class="material-icons left upload_icon">arrow_upward</i>
+            </button>
+
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="../assets/libraries/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="../assets/libraries/materialize.js"></script>
+<script type="text/javascript" src="assets/js/main.js"></script>
+
+</body>
+</html>
